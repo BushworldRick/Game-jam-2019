69595dcd08b57470d4b973098fb01960f350b834
BushworldRick <ethancreeds@gmail.com>
2019-04-05 23:45:50 -0400

    attempted fix

--- Player.py
+++ Player.py
@@ -10,10 +10,10 @@ class Player:
         self.screenheight = screen_dems[1]
 
         # player stuff
-        self.pos = [self.screenwidth//2,500]
+        self.player_pos = [self.screenwidth//2,500]
         self.speed = 150
         self.player_hitbox = ship_directions["framewidth"]//2
-        self.bullets = []   # bullet class objects
+        self.bullet_list = []   # bullet class objects
         self.frame_row = 6
         self.frame_column = 0
 
@@ -25,25 +25,29 @@ class Player:
     def update(self, dt):
         self.dt = dt
 
+        #bullet Update
+        for b in self.bullet_list:
+            b.update(dt)
+
         # keep on screen code
-        if self.pos[0] < 0 + self.player_hitbox:
-            self.pos[0] = 0 + self.player_hitbox
-        if self.pos[0] > self.screenwidth - self.player_hitbox:
-            self.pos[0] = self.screenwidth - self.player_hitbox
-        if self.pos[1] < 0 + (self.player_hitbox * 2):
-            self.pos[1] = 0 + (self.player_hitbox * 2)
-        if self.pos[1] > self.screenheight - (self.player_hitbox *  2):
-            self.pos[1] = self.screenheight - (self.player_hitbox * 2)
+        if self.player_pos[0] < 0 + self.player_hitbox:
+            self.player_pos[0] = 0 + self.player_hitbox
+        if self.player_pos[0] > self.screenwidth - self.player_hitbox:
+            self.player_pos[0] = self.screenwidth - self.player_hitbox
+        if self.player_pos[1] < 0 + (self.player_hitbox * 2):
+            self.player_pos[1] = 0 + (self.player_hitbox * 2)
+        if self.player_pos[1] > self.screenheight - (self.player_hitbox *  2):
+            self.player_pos[1] = self.screenheight - (self.player_hitbox * 2)
 
     def input(self, evt, keys):
         # Moving player_ship
         if keys[pygame.K_a] or keys[pygame.K_LEFT]:
-            self.pos[0] -= self.speed * self.dt
+            self.player_pos[0] -= self.speed * self.dt
             self.frame_row = 4
             self.frame_column = 1
 
         elif keys[pygame.K_d] or keys[pygame.K_RIGHT]:
-            self.pos[0] += self.speed * self.dt
+            self.player_pos[0] += self.speed * self.dt
             self.frame_row = 6
             self.frame_column = 1
         else:
@@ -51,23 +55,25 @@ class Player:
             self.frame_column = 0
 
         if keys[pygame.K_w] or keys[pygame.K_UP]:
-            self.pos[1] -= self.speed * self.dt
+            self.player_pos[1] -= self.speed * self.dt
         if keys[pygame.K_s] or keys[pygame.K_DOWN]:
-            self.pos[1] += self.speed * self.dt
+            self.player_pos[1] += self.speed * self.dt
+
+        # make bullet list
         if evt.type == pygame.KEYDOWN:
             if evt.key == pygame.K_SPACE:
-                self.bullet_list.append(Bullet(self.Player.pos, 1))
-                self.bullet_list.append(Bullet(self.Player.pos, 2))
-                self.bullet_list.append(Bullet(self.Player.pos, 3))
+                self.bullet_list.append(Bullet(self.player_pos, 1))
+                self.bullet_list.append(Bullet(self.player_pos, 2))
+                self.bullet_list.append(Bullet(self.player_pos, 3))
 
     def draw(self,surf):
         # temorary player
-        pygame.draw.circle(surf, (0,255,0), (int(self.pos[0]), int(self.pos[1])), self.player_hitbox, 1)
+        pygame.draw.circle(surf, (0,255,0), (int(self.player_pos[0]), int(self.player_pos[1])), self.player_hitbox, 1)
 
         rect = (self.img_w * self.frame_column, self.img_h * self.frame_row,
         self.img_w, self.img_h)
 
-        surf.blit(self.img, (int(self.pos[0] - (self.img_w/2 + 15)),int(self.pos[1] - (self.img_h/2 + 15))), rect)
-
         for i in self.bullet_list:
             i.draw(win)
+
+        surf.blit(self.img, (int(self.player_pos[0] - (self.img_w/2 + 15)),int(self.player_pos[1] - (self.img_h/2 + 15))), rect)
--- bullet.py
+++ bullet.py
@@ -5,25 +5,26 @@ class Bullet:
     def __init__(self, position, dir=1):
         self.pos = position
         self.dir = dir
+        self.speed = 300
         self.rad = 3
 
     def update(self, dt):
         if self.dir == 2:
-            self.pos[1] -= self.rad * dt
+            self.pos[1] -= self.speed * dt
         elif self.dir == 1:
-            self.pos[0] += -self.rad * dt
-            self.pos[1] -= self.rad * dt
+            self.pos[0] += -self.speed * dt
+            self.pos[1] -= self.speed * dt
         elif self.dir == 3:
-            self.pos[0] += self.rad * dt
-            self.pos[1] -= self.rad * dt
+            self.pos[0] += self.speed * dt
+            self.pos[1] -= self.speed * dt
         elif self.dir == -2:
-            self.pos[1] += self.rad * dt
+            self.pos[1] += self.speed * dt
         elif self.dir == -1:
-            self.pos[0] -= -self.rad * dt
-            self.pos[1] += self.rad * dt
+            self.pos[0] -= -self.speed * dt
+            self.pos[1] += self.speed * dt
         elif self.dir == -3:
-            self.pos[0] -= self.rad * dt
-            self.pos[1] += self.rad * dt
+            self.pos[0] -= self.speed * dt
+            self.pos[1] += self.speed * dt
 
     def draw(self, win):
         pygame.draw.circle(win, (0, 255, 0), (int(self.pos[0]), int(self.pos[1])), self.rad)
