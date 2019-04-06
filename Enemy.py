6dedd1ecd5fd4b0986b67189955e2b6b8db760c6
Drod2099 <37273083+Drod2099@users.noreply.github.com>
2019-04-06 05:44:40 -0400

    Added spritesheets and fixed blit for enemy

--- Enemy.py
+++ Enemy.py
@@ -19,4 +19,4 @@ class Enemy:
         self.mPos[0] = math.sin(self.mPos[1] * .05) * self.mSin_width + self.mid_x
 
     def draw(self, win):
-        win.blit(ENEMY, (int(self.mPos[0]), int(self.mPos[1])))
+        win.blit(ENEMY, (int(self.mPos[0]), int(self.mPos[1])), (0, 0, 35, 35))
